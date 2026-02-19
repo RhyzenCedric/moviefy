88ab53a4bdb01a9c8377f4abdb6b75bb909c23c0
RhyzenCedric <rcedricgarcia@gmail.com>
2026-02-19 17:02:33 +0800

    Resolved use client error again

--- src/app/AudioPlayer.tsx
+++ src/app/AudioPlayer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef, useState } from 'react';
 import type { iTunesSong } from '../../.next/dev/types/itunes';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
